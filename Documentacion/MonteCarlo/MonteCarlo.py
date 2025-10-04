6e4659af8457c5393e85bc0a479427eb4491b758
CocoUrbina <edurbinaq@gmail.com>
2025-10-04 09:38:48 -0600

    New files added

---
+++ Documentacion/MonteCarlo/MonteCarlo.py
@@ -0,0 +1,13 @@
+def montecarlo(x):
+    """ Lorem Ipsum
+
+    Examples:
+        >>> montecarlo(x)
+        2.0
+
+    Args:
+        x (float): Argumento unico.
+
+    Returns:
+        aprox (float): Valor aproximado de la integral.
+    """
