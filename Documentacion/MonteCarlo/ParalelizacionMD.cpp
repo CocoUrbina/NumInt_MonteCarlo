02d3e259fab307b0274f0bab8825877c783a945b
Ervin Villalta <ervin.v21102.0@gmail.com>
2025-12-04 01:01:38 -0600

    Actualización #1 de paralelización

--- Documentacion/MonteCarlo/ParalelizacionMD.cpp
+++ Documentacion/MonteCarlo/ParalelizacionMD.cpp
@@ -17,7 +17,7 @@ double func(const std::vector<double>& punto) {
 int main(int argc, char* argv[]) {
 
     if(argc != 9){
-        std::cerr << "Usage: " << argv[0] << " --li [límite inferior] --ls [límite superior] --d [número de dimensiones] --n [cantidad de puntos]" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " --li [límite inferior] --ls [límite superior] --d [número de dimensiones] --n [interacciones]" << std::endl;
         exit(1);
 
 
@@ -36,21 +36,46 @@ int main(int argc, char* argv[]) {
     
     // semilla
     unsigned int seed = 12345;
-    
+
+    //Inicialización del ambiente MPI
     int size, rank;
     MPI_Init(NULL, NULL);
 
-    double time_1 = MPI_Wtime()
+    double time_1 = MPI_Wtime();
+
+    //Tamaño y rango
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    //Subdivisión del rango de trabajo
+    int nlocal = N / size;
+    int rest = N % size;
+
+    if (rest && rank < rest){
+    
+    	nlocal++;
+    
+    }
+
+    int start = nlocal * rank;
+
+    if (rest && rank >= rest){
+
+    	start += rest;
+
+    }
+
+    int end = start + nlocal;
 
     // Generador Mersenne Twister 
-    std::mt19937 generador(seed);
+    std::mt19937 generador(seed + rank);
     
     // Variables para el cálculo
-    double suma_final = 0.0;
-    double suma_final2 = 0.0;  // Para calcular varianza
+    double suma_local = 0.0;
+    double suma_cuadrados_local = 0.0;  // Para calcular varianza
     
     // Bucle principal de Monte Carlo
-    for (int i = 0; i < N; i++) {
+    for (int i = start; i < end; i++) {
         // Generar punto multidimensional (creo que es mejor evitar traer uniform... del std library?
         std::vector<double> punto(dimensiones);
         for (int d = 0; d < dimensiones; d++) {
@@ -66,13 +91,20 @@ int main(int argc, char* argv[]) {
         double valor_final = func(punto);
         
         // Acumular para cálculos finales después
-        suma_final += valor_final;
-        suma_final2 += valor_final * valor_final;
+        suma_local += valor_final;
+        suma_cuadrados_local += valor_final * valor_final;
     }
+
+    double suma_global, suma_cuadrados_global;
     
+    MPI_Reduce(&suma_local, &suma_global, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&suma_cuadrados_local, &suma_cuadrados_global, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    double time_2 = MPI_Wtime();
+
     // Cálculos finales
-    double promedio = suma_final / N;
-    double promedio_cuadrado = suma_final2 / N;
+    double promedio = suma_global / N;
+    double promedio_cuadrado = suma_cuadrados_global / N;
     double varianza = promedio_cuadrado - promedio * promedio;
     
     // Volumen del hipercubo
@@ -96,6 +128,9 @@ int main(int argc, char* argv[]) {
     std::cout << "Error estimado: " << error << std::endl;
     std::cout << "Varianza de f: " << varianza << std::endl;
     std::cout << "Escala del error: O(N^{-1/2})" << std::endl;
+    std::cout << "Tiempo: " << time_2 - time_1 << std::endl;
     
+    MPI_Finalize();
+
     return 0;
 }
